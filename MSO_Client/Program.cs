cb52a19de56d24bd598764211b02dbbe49170296
Divanchik <d-sh-2016@list.ru>
2022-03-25 14:35:05 +0400

    greeter test

--- MSO_Client/Program.cs
+++ MSO_Client/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
 
 namespace MSO_Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest{Name = "GreeterClient"});
+            Console.WriteLine(reply.Message);
+            Console.ReadKey();
         }
     }
 }
