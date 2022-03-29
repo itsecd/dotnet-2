a1025c29bd6392db8123c09dbb5cdbf40bd70ae9
Divanchik <d-sh-2016@list.ru>
2022-03-29 11:14:34 +0400

    service update

--- MSO_Server/Services/GameService.cs
+++ MSO_Server/Services/GameService.cs
@@ -16,9 +16,29 @@ namespace MSO_Server
             _data = data;
             _data.Load();
         }
-        public override Task CreateRoom(IAsyncStreamReader<PlayerMessage> requestStream, IServerStreamWriter<ServerMessage> responseStream, ServerCallContext context)
+        public override async Task CreateRoom(IAsyncStreamReader<PlayerMessage> requestStream, IServerStreamWriter<ServerMessage> responseStream, ServerCallContext context)
         {
-            return base.CreateRoom(requestStream, responseStream, context);
+            if (!await requestStream.MoveNext()) return;
+
+            // create room
+            var playerMessage = requestStream.Current;
+            int res = _data.Create(playerMessage.Name, playerMessage.State);
+            await responseStream.WriteAsync(new ServerMessage{Text = "new room?", State = res});
+            _data.Dump();
+
+            // pre game
+            while (true)
+            {
+                await requestStream.MoveNext();
+                playerMessage = requestStream.Current;
+                if (playerMessage.Text == "get_players")
+                    await responseStream.WriteAsync(new ServerMessage{Text = "not implemented", State = 0});
+                if (playerMessage.Text == "leave")
+                {
+                    await responseStream.WriteAsync(new ServerMessage{Text = "OK", State = 0});
+                    break;
+                }
+            }
         }
         public override Task JoinRoom(IAsyncStreamReader<PlayerMessage> requestStream, IServerStreamWriter<ServerMessage> responseStream, ServerCallContext context)
         {
