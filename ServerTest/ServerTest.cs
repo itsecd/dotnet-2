a86bbcf47415e8bc99be287b0dadc71e9e5b8848
Divanchik <d-sh-2016@list.ru>
2022-04-08 11:03:34 +0400

    Дописаны тесты

--- ServerTest/ServerTest.cs
+++ ServerTest/ServerTest.cs
@@ -2,6 +2,7 @@ using System;
 using Xunit;
 using MinesweeperServer.Database;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace ServerTest
 {
@@ -17,19 +18,48 @@ namespace ServerTest
         [Fact]
         public void ScoreTest()
         {
-            GameRepository repo = new(null);
+            var configurationRoot = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            GameRepository repo = new(configurationRoot);
             repo.TryAdd("username");
             Assert.True(repo.CalcScore("username", "win"));
             Assert.True(repo.CalcScore("username", "win"));
-            Assert.Equal(repo["username"].TotalScore, 20);
-            Assert.Equal(repo["username"].WinStreak, 2);
-            Assert.Equal(repo["username"].WinCount, 2);
-            Assert.Equal(repo["username"].LoseCount, 0);
+            Assert.Equal(20, repo["username"].TotalScore);
+            Assert.Equal(2, repo["username"].WinStreak);
+            Assert.Equal(2, repo["username"].WinCount);
+            Assert.Equal(0, repo["username"].LoseCount);
             Assert.True(repo.CalcScore("username", "lose"));
-            Assert.Equal(repo["username"].TotalScore, 8);
-            Assert.Equal(repo["username"].WinStreak, 0);
-            Assert.Equal(repo["username"].WinCount, 2);
-            Assert.Equal(repo["username"].LoseCount, 1);
+            Assert.Equal(8, repo["username"].TotalScore);
+            Assert.Equal(0, repo["username"].WinStreak);
+            Assert.Equal(2, repo["username"].WinCount);
+            Assert.Equal(1, repo["username"].LoseCount);
+        }
+    }
+    public class NetworkTest
+    {
+        [Fact]
+        public void ConnectionTest()
+        {
+            GameNetwork network = new();
+            Assert.True(network.Join("username", null));
+            Assert.False(network.Join("username", null));
+            Assert.True(network.IsConnected("username"));
+            Assert.True(network.Leave("username"));
+            Assert.False(network.Leave("username"));
+            Assert.False(network.IsConnected("username"));
+        }
+        [Fact]
+        public void StatesTest()
+        {
+            GameNetwork network = new();
+            network.Join("username", null);
+            network.Join("destroyer", null);
+            Assert.True(network.AllStates("lobby"));
+            network.DeclareWin("destroyer");
+            Assert.Equal("win", network.GetPlayerState("destroyer"));
+            Assert.Equal("lose", network.GetPlayerState("username"));
+
+            network.SetPlayerState("username", "lobby");
+            Assert.Equal("lobby", network.GetPlayerState("username"));
         }
     }
 }
