dbec40206873ee226ee0807a378bfeaaaa563b96
Ivan <pravdin.biss@gmail.com>
2022-04-23 20:46:53 +0400

    add repository

---
+++ TelegramBotServer/Repository/IEventRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelegramBotServer.Model;
+
+namespace TelegramBotServer.Repository
+{
+    public interface IEventRepository
+    {
+        public int AddEvent(Event newEvent);
+        public void RemoveEvent(Event someEvent);
+        public void ChangeEvent(int id, Event newEvent);
+        public Event GetEvent(int id);
+        IEnumerable<Event> GetEvents();
+    }
+}
---
+++ TelegramBotServer/Repository/XmlEventRepository.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TelegramBotServer.Model;
+using System.Linq;
+
+namespace TelegramBotServer.Repository
+{
+    public class XmlEventRepository : IEventRepository
+    {
+        private readonly string _filePath;
+        private List<Event> _events;
+        
+        public XmlEventRepository(IConfiguration config)
+        {
+            _filePath = config["XmlDatabaseFile"];
+        }
+
+        public int AddEvent(Event newEvent)
+        {
+            ReadFile();
+            _events.Add(newEvent);
+            WriteFile();
+            return newEvent.Id;
+        }
+
+        public void ChangeEvent(int id, Event newEvent)
+        {
+            ReadFile();
+            var index  = _events.IndexOf(_events.Where(e => e.Id == id).FirstOrDefault());
+            _events[index] = newEvent;
+            WriteFile();
+        }
+
+        public Event GetEvent(int id)
+        {
+            ReadFile();
+            return _events.Where(e => e.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Event> GetEvents()
+        {
+            ReadFile();
+            return _events;
+        }
+
+        public void RemoveEvent(Event someEvent)
+        {
+            ReadFile();
+            _events.Remove(someEvent);
+            WriteFile();
+        }
+
+        private void ReadFile()
+        {
+            if (_events != null)
+                return;
+
+            if (!File.Exists(_filePath))
+            {
+                _events = new();
+                return;
+            }
+
+            var xmlSerializer = new XmlSerializer(typeof(List<Event>));
+            using var fileStream = new FileStream(_filePath, FileMode.Open);
+            _events = (List<Event>)xmlSerializer.Deserialize(fileStream);
+        }
+
+        private void WriteFile()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(List<Event>));
+            using var fileStream = new FileStream(_filePath, FileMode.Create);
+            xmlSerializer.Serialize(fileStream, _events);
+        }
+
+    }
+}
