72a9453a9af0b010093b006f62b54d937c10f9f5
Ivan <pravdin.biss@gmail.com>
2022-04-25 23:43:01 +0400

    complete all methods for EventController

--- TelegramBotServer/Controllers/EventController.cs
+++ TelegramBotServer/Controllers/EventController.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TelegramBotServer.Model;
 using TelegramBotServer.Repository;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TelegramBotServer.Controllers
 {
@@ -43,14 +39,16 @@ namespace TelegramBotServer.Controllers
 
         // PUT api/<EventController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Event someEvent)
         {
+            _repository.ChangeEvent(id, someEvent);
         }
 
         // DELETE api/<EventController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            _repository.RemoveEvent(id);
         }
     }
 }
