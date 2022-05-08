8a45b71bc721b18572896a107441f60e7516bc2d
Ivan <pravdin.biss@gmail.com>
2022-05-08 22:18:00 +0400

    add data anatations

--- TelegramBotServer/Model/Event.cs
+++ TelegramBotServer/Model/Event.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TelegramBotServer.Model
 {
     public class Event
     {
+        [Range(1, int.MaxValue, ErrorMessage = "Id should be greater than or equal to 1")]
         public int Id { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "SubscriberId should be greater than or equal to 1")]
         public int SubscriberId { get; set; }
         public DateTime Deadline { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Reminder should be greater than 0")]
         public int Reminder { get; set; }
+        public bool Notified { get; set; }
     }
 }
--- TelegramBotServer/Model/Subscriber.cs
+++ TelegramBotServer/Model/Subscriber.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TelegramBotServer.Model
 {
     public class Subscriber
     {
+        [Range(1, int.MaxValue, ErrorMessage = "Id should be greater than or equal to 1")]
         public int Id { get; set; }
-
+        [Range(1, long.MaxValue, ErrorMessage = "UserId should be greater than or equal to 1")]
         public long UserId { get; set; }
+        [Range(1, long.MaxValue, ErrorMessage = "ChatId should be greater than or equal to 1")]
         public long ChatId { get; set; }
         public List<int>? EventsId { get; set; }
     }
