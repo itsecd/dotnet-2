0bdcdddf1861fef8f9375aee915cf691ac6a20c8
Ivan <pravdin.biss@gmail.com>
2022-05-08 13:28:42 +0400

    add db context

---
+++ TelegramBotServer/DatabaseContext/UsersContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TelegramBotServer.Model;
+
+namespace TelegramBotServer.DatabaseContext
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Subscriber> Subscribers { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+        public UsersContext(DbContextOptions<UsersContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+        
+    }
+}
