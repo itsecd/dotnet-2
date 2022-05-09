9800c59f47dc5c70e8164a2cbcd12aa15419ccff
Ivan <pravdin.biss@gmail.com>
2022-05-09 23:39:15 +0400

    create test project

---
+++ TelegramBotServer.Tests/ControllersTests/EventControllerTests.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBotServer.Controllers;
+using TelegramBotServer.Model;
+using TelegramBotServer.Repository;
+using Xunit;
+
+namespace TelegramBotServer.Tests.ControllersTests
+{
+    public class EventControllerTests
+    {
+        const int eventId = 5;
+        const int subId = 9;
+        const bool notified = false;
+        const int unreachableReturnStatus = 404;
+        const int reachableReturnStatus = 200;
+        readonly DateTime validDeadline;
+
+        public EventControllerTests()
+        {
+            validDeadline = DateTime.Now + TimeSpan.FromMinutes(30);
+        }
+
+        [Fact]
+        public void GetReachiableEvent()
+        {
+            var mockRepo = new Mock<IEventRepository>();
+            var someEvent = new Event
+            {
+                Id = eventId,
+                SubscriberId = subId,
+                Notified = notified,
+                Deadline = validDeadline
+            };
+            mockRepo.Setup(r => r.GetEvent(eventId)).Returns(someEvent);
+            var controller = new EventController(mockRepo.Object);
+
+            var result = controller.Get(eventId).Value;
+
+            Assert.Equal(result, someEvent);
+        }
+
+        [Fact]
+        public void GetUnreachiableEvent()
+        {
+            var mockRepo = new Mock<IEventRepository>();
+            mockRepo.Setup(r => r.GetEvent(eventId)).Returns((Event)null);
+            var controller = new EventController(mockRepo.Object);
+
+            var result = controller.Get(eventId).Result as ObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(unreachableReturnStatus, result.StatusCode);
+        }
+
+        [Fact]
+        public void PutReachableEvent()
+        {
+            var mockRepo = new Mock<IEventRepository>();
+            var someEvent = new Event
+            {
+                Id = eventId,
+                SubscriberId = subId,
+                Notified = notified,
+                Deadline = validDeadline
+            };
+            mockRepo.Setup(r => r.ChangeEvent(eventId, someEvent)).Returns(true);
+            var controller = new EventController(mockRepo.Object);
+
+            var result = controller.Put(eventId, someEvent) as OkResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(reachableReturnStatus, result.StatusCode);
+        }
+
+        [Fact]
+        public void PutUnreachableEvent()
+        {
+            var mockRepo = new Mock<IEventRepository>();
+            var someEvent = new Event
+            {
+                Id = eventId,
+                SubscriberId = subId,
+                Notified = notified,
+                Deadline = validDeadline
+            };
+            mockRepo.Setup(r => r.ChangeEvent(eventId, someEvent)).Returns(false);
+            var controller = new EventController(mockRepo.Object);
+
+            var result = controller.Put(eventId, someEvent) as ObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(unreachableReturnStatus, result.StatusCode);
+        }
+
+        [Fact]
+        public void DeleteUnreachableEvent()
+        {
+            var mockRepo = new Mock<IEventRepository>();
+            mockRepo.Setup(r => r.RemoveEvent(eventId)).Returns(false);
+            var controller = new EventController(mockRepo.Object);
+
+            var result = controller.Delete(eventId) as ObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(unreachableReturnStatus, result.StatusCode);
+        }
+
+        [Fact]
+        public void DeleteReachableEvent()
+        {
+            var mockRepo = new Mock<IEventRepository>();
+            mockRepo.Setup(r => r.RemoveEvent(eventId)).Returns(true);
+            var controller = new EventController(mockRepo.Object);
+
+            var result = controller.Delete(eventId) as OkResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(reachableReturnStatus, result.StatusCode);
+        }
+    }
+}
---
+++ TelegramBotServer.Tests/ControllersTests/SubscriberControllerTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBotServer.Controllers;
+using TelegramBotServer.Model;
+using TelegramBotServer.Repository;
+using Xunit;
+
+namespace TelegramBotServer.Tests.ControllersTests
+{
+    public class SubscriberControllerTests
+    {
+        const int subId = 5;
+        const int chatId = 9;
+        readonly int[] eventsId = { 1, 2, 3 };
+        const int unreachableReturnStatus = 404;
+        const int reachableReturnStatus = 200;
+
+        [Fact]
+        public void GetReachiableSub()
+        {
+            var mockRepo = new Mock<ISubscriberRepository>();
+            var subscriber = new Subscriber
+            {
+                Id = subId,
+                UserId = chatId,
+                ChatId = chatId,
+                EventsId = eventsId.ToList()
+            };
+            mockRepo.Setup(r => r.GetSubscriber(subId)).Returns(subscriber);
+            var controller = new SubscriberController(mockRepo.Object);
+
+            var result = controller.Get(subId).Value;
+
+            Assert.Equal(result, subscriber);
+        }
+
+        [Fact]
+        public void GetUnreachiableSub()
+        {
+            var mockRepo = new Mock<ISubscriberRepository>();
+            mockRepo.Setup(r => r.GetSubscriber(subId)).Returns((Subscriber)null);
+            var controller = new SubscriberController(mockRepo.Object);
+
+            var result = controller.Get(subId).Result as ObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(unreachableReturnStatus, result.StatusCode);
+        }
+
+        [Fact]
+        public void PutReachableSub()
+        {
+            var mockRepo = new Mock<ISubscriberRepository>();
+            var subscriber = new Subscriber
+            {
+                Id = subId,
+                UserId = chatId,
+                ChatId = chatId,
+                EventsId = eventsId.ToList()
+            };
+            mockRepo.Setup(r => r.ChangeSubscriber(subId, subscriber)).Returns(true);
+            var controller = new SubscriberController(mockRepo.Object);
+
+            var result = controller.Put(subId, subscriber) as OkResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(reachableReturnStatus, result.StatusCode);
+        }
+
+        [Fact]
+        public void PutUnreachableSub()
+        {
+            var mockRepo = new Mock<ISubscriberRepository>();
+            var subscriber = new Subscriber
+            {
+                Id = subId,
+                UserId = chatId,
+                ChatId = chatId,
+                EventsId = eventsId.ToList()
+            };
+            mockRepo.Setup(r => r.ChangeSubscriber(subId, subscriber)).Returns(false);
+            var controller = new SubscriberController(mockRepo.Object);
+
+            var result = controller.Put(subId, subscriber) as ObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(unreachableReturnStatus, result.StatusCode);
+        }
+
+        [Fact]
+        public void DeleteUnreachableEvent()
+        {
+            var mockRepo = new Mock<IEventRepository>();
+            mockRepo.Setup(r => r.RemoveEvent(subId)).Returns(false);
+            var controller = new EventController(mockRepo.Object);
+
+            var result = controller.Delete(subId) as ObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(unreachableReturnStatus, result.StatusCode);
+        }
+
+        [Fact]
+        public void DeleteReachableEvent()
+        {
+            var mockRepo = new Mock<IEventRepository>();
+            mockRepo.Setup(r => r.RemoveEvent(subId)).Returns(true);
+            var controller = new EventController(mockRepo.Object);
+
+            var result = controller.Delete(subId) as OkResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(reachableReturnStatus, result.StatusCode);
+        }
+    }
+}
