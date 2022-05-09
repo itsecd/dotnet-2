61b22780c79d6e3e7ccdb1a533bc2c4143af727b
Ivan <pravdin.biss@gmail.com>
2022-05-09 16:55:57 +0400

    fix

--- TelegramBotServer/Model/Event.cs
+++ TelegramBotServer/Model/Event.cs
@@ -5,12 +5,12 @@ namespace TelegramBotServer.Model
 {
     public class Event
     {
-        [Range(1, int.MaxValue, ErrorMessage = "Id should be greater than or equal to 1")]
+        [Range(0, int.MaxValue, ErrorMessage = "Id should be positive number")]
         public int Id { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "SubscriberId should be greater than or equal to 1")]
         public int SubscriberId { get; set; }
         public DateTime Deadline { get; set; }
-        [Range(0, int.MaxValue, ErrorMessage = "Reminder should be greater than 0")]
+        [Range(0, int.MaxValue, ErrorMessage = "Reminder should be positive number")]
         public int Reminder { get; set; }
         public bool Notified { get; set; }
     }
--- TelegramBotServer/Model/Subscriber.cs
+++ TelegramBotServer/Model/Subscriber.cs
@@ -6,7 +6,7 @@ namespace TelegramBotServer.Model
 {
     public class Subscriber
     {
-        [Range(1, int.MaxValue, ErrorMessage = "Id should be greater than or equal to 1")]
+        [Range(0, int.MaxValue, ErrorMessage = "Id should be positive number")]
         public int Id { get; set; }
         [Range(1, long.MaxValue, ErrorMessage = "UserId should be greater than or equal to 1")]
         public long UserId { get; set; }
