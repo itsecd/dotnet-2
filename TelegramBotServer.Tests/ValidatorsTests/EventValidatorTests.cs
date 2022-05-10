1f0982b3b617898c1b6eb29d3adf95cae3998b18
Ivan <pravdin.biss@gmail.com>
2022-05-10 15:19:34 +0400

    add validators test

---
+++ TelegramBotServer.Tests/ValidatorsTests/EventValidatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBotServer.Model;
+using TelegramBotServer.Validators;
+using Xunit;
+
+namespace TelegramBotServer.Tests.ValidatorsTests
+{
+    public class EventValidatorTests
+    {
+        const int eventId = 5;
+        const int subId = 9;
+        const bool notified = false;
+        readonly DateTime validDeadline;
+        readonly DateTime invalidDeadline;
+
+        public EventValidatorTests()
+        {
+            validDeadline = DateTime.Now + TimeSpan.FromMinutes(30);
+            invalidDeadline = DateTime.Now - TimeSpan.FromMinutes(30);
+        }
+
+        [Fact]
+        public void ValideValidEvent()
+        {
+            var validEvent = new Event
+            {
+                Id = eventId,
+                SubscriberId = subId,
+                Notified = notified,
+                Deadline = validDeadline
+            };
+            var validator = new EventValidator();
+
+            var result = validator.Validate(validEvent);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ValideInvalidEvent()
+        {
+            var invalidEvent = new Event
+            {
+                Id = eventId,
+                SubscriberId = subId,
+                Notified = notified,
+                Deadline = invalidDeadline
+            };
+            var validator = new EventValidator();
+
+            var result = validator.Validate(invalidEvent);
+
+            Assert.False(result);
+        }
+    }
+}
---
+++ TelegramBotServer.Tests/ValidatorsTests/SubscriberValidatorTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBotServer.Model;
+using TelegramBotServer.Validators;
+using Xunit;
+
+namespace TelegramBotServer.Tests.ValidatorsTests
+{
+    public class SubscriberValidatorTests
+    {
+        const int subId = 5;
+        const int chatId = 9;
+        readonly int[] eventsId = { 1, 2, 3 };
+
+        [Fact]
+        public void ValidateValidSub()
+        {
+            var validSub = new Subscriber
+            {
+                Id = subId,
+                UserId = chatId,
+                ChatId = chatId,
+                EventsId = eventsId.ToList()
+            };
+            var validator = new SubscriberValidator(GetEvents());
+
+            var result = validator.Validate(validSub);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ValidateInvalidSub()
+        {
+            var validSub = new Subscriber
+            {
+                Id = subId,
+                UserId = chatId,
+                ChatId = chatId,
+                EventsId = eventsId.ToList()
+            };
+            var validator = new SubscriberValidator(new List<Event> ());
+
+            var result = validator.Validate(validSub);
+
+            Assert.False(result);
+        }
+
+        List<Event> GetEvents()
+        {
+            var events = new List<Event>();
+
+            foreach(var eventId in eventsId)
+            {
+                events.Add(new Event { Id = eventId });
+            }
+            return events;
+        }
+    }
+}
