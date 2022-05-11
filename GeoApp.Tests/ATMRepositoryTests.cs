04c5f49a03a627a36de7e991960eef541704ac6d
VladaHaustova <vlada.khaustova@yandex.ru>
2022-05-11 11:03:29 +0400

    Remove usings

--- GeoApp.Tests/ATMRepositoryTests.cs
+++ GeoApp.Tests/ATMRepositoryTests.cs
@@ -1,9 +1,7 @@
 ﻿using GeoApp.Repository;
 using GeoApp.Model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
