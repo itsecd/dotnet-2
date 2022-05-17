bd2c8ae5253b892d6ab0dc36b2f3a392d8deb0be
Роман Шафранюк <romanshafranuk@gmail.com>
2022-05-17 20:35:29 +0400

    Попытка добавить новую команду

--- ChatClient/MainWindow.xaml.cs
+++ ChatClient/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChatClient.ViewModel;
+using Grpc.Net.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,10 +25,15 @@ namespace ChatClient
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
 
         private void userName_TextChanged(object sender, TextChangedEventArgs e)
         {
+            //(DataContext as MainViewModel).Rooms
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            ChatServer.ChatRoom.ChatRoomClient cl = new ChatServer.ChatRoom.ChatRoomClient(channel);
 
         }
     }
---
+++ ChatClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reactive;
+using System.Runtime.CompilerServices;
+namespace ChatClient.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<ChatRoom> ?_rooms;
+        public ObservableCollection<ChatRoom> Rooms
+        {
+            get { return _rooms; }
+            set
+            {
+                _rooms = value;
+                OnPropertyChanged(nameof(Rooms));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public ReactiveCommand<Unit, Unit> CreateCommand { get; }
+        public MainViewModel() { 
+            CreateCommand = ReactiveCommand.Create(CreateImpl);
+        }
+
+        private void CreateImpl() { 
+            DialogWindow dialogWindow = new DialogWindow();
+            dialogWindow.ShowDialog();
+        }
+    }
+}
