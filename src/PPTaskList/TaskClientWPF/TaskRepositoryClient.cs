c3252fe6c66755c7d64d205441f1e4d0df48b121
Daria-creator-lab <pochtadlyunivera@mail.ru>
2022-05-22 15:49:42 +0400

    Создан клиент на WPF

---
+++ src/PPTaskList/TaskClientWPF/TaskRepositoryClient.cs
@@ -0,0 +1,20 @@
+﻿using TaskClientWPF.Properties;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskClientWPF
+{
+    public class TaskRepositoryClient
+    {
+        private readonly TaskClient _taskClient;
+
+        public TaskRepositoryClient()
+        {
+            var httpClient = new HttpClient();
+            var baseUrl = Settings.Default.OpenApiClient;
+            _taskClient = new TaskClient(baseUrl,httpClient);
+        }
+
+    }
+}
