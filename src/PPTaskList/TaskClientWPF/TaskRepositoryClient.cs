878b5ab7d98427f6835cf4a636ccf8145f2b8c45
Daria-creator-lab <pochtadlyunivera@mail.ru>
2022-05-22 16:13:46 +0400

    Создана обертка для Executor

--- src/PPTaskList/TaskClientWPF/TaskRepositoryClient.cs
+++ src/PPTaskList/TaskClientWPF/TaskRepositoryClient.cs
@@ -7,14 +7,43 @@ namespace TaskClientWPF
 {
     public class TaskRepositoryClient
     {
-        private readonly TaskClient _taskClient;
+        private readonly TaskClient _client;
 
         public TaskRepositoryClient()
         {
             var httpClient = new HttpClient();
             var baseUrl = Settings.Default.OpenApiClient;
-            _taskClient = new TaskClient(baseUrl,httpClient);
+            _client = new TaskClient(baseUrl,httpClient);
         }
 
+        public Task<ICollection<Executor>> GetExecutorsAsync()
+        {
+            return _client.ExecutorAllAsync();
+        }
+
+        public Task PostExecutorAsync(ExecutorDto executor)
+        {
+            return _client.ExecutorAsync(executor);
+        }
+
+        public Task<Executor> GetExecutorAsync(int id)
+        {
+            return _client.Executor2Async(id);
+        }
+
+        public Task UpdateExecutorAsync(int id, ExecutorDto executor)
+        {
+            return _client.Executor3Async(id, executor);
+        }
+
+        public Task RemoveExecutorAsync(int id)
+        {
+            return _client.Executor4Async(id);
+        }
+
+        public Task<ICollection<TaskDto>> GetExecutorTasksAsync(int id)
+        {
+            return _client.TasksAsync(id);
+        }
     }
 }
