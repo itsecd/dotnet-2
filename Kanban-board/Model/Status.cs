4989664182b33763cb98364d1639d95ed4748ec8
VolumeCore <volumejke@gmail.com>
2022-05-23 20:30:46 +0400

    Add Models, disable Cors policy, include Xml description

---
+++ Kanban-board/Model/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace Kanban_board.Model
+{
+
+    public class Status
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+    }
+}
---
+++ Kanban-board/Model/Ticket.cs
@@ -0,0 +1,9 @@
+﻿namespace Kanban_board.Model
+{
+    public class Ticket
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
