4716e7f4d1e796b8e37da43b46c921ddf3f9c924
Oleg Chudilin <73231661+EraserH@users.noreply.github.com>
2022-05-25 11:52:49 +0400

    использован await using declaration, код почищен, убрана лишняя инициализация списка

--- ChatServer/Serializers/GroupMessageSerializer.cs
+++ ChatServer/Serializers/GroupMessageSerializer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.IO;
@@ -15,21 +13,15 @@ namespace ChatServer.Serializers
             var messages = await DeserializeMessage(groupMessage.Group);
             messages.Add(groupMessage);
 
-            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
-            {
-                await JsonSerializer.SerializeAsync(fs, messages);
-            }
+            await using var fs = new FileStream(fileName, FileMode.OpenOrCreate);
+            await JsonSerializer.SerializeAsync(fs, messages);
         }
 
         public static async Task<List<GroupMessage>> DeserializeMessage(string groupName)
         {
             var fileName = "RoomDataBases/" + groupName + ".json";
-            var deserializedMessages = new List<GroupMessage>();
-
-            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
-            {
-                deserializedMessages = await JsonSerializer.DeserializeAsync<List<GroupMessage>>(fs);
-            }
+            await using var fs = new FileStream(fileName, FileMode.OpenOrCreate);
+            var deserializedMessages = await JsonSerializer.DeserializeAsync<List<GroupMessage>>(fs);
 
             return deserializedMessages;
         }
