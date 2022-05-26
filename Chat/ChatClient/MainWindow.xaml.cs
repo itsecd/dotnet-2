4e037dae5b09c3bda80ae8fa87e242d6aea6ac47
nikitonya <nik_osipov_2014@mail.ru>
2022-05-27 02:32:40 +0400

    an authorization and registration window has been created

---
+++ Chat/ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginBox.Text == "admin" && PasswordBox.Password == "123")
+            {
+                //Open(ContactsScreen);
+            }
+            else
+            {
+                LoginMessageBlock.Text = "Wrong login or Password!";
+                LoginMessageBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            Open(RegisterScreen);
+        }
+
+        private void Open(Border screen)
+        {
+            LoginScreen.Visibility = Visibility.Hidden;
+
+            screen.Visibility = Visibility.Visible;
+        }
+    }
+}
