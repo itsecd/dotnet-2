90b56efb929945e3be6a4fb1b2f0d72b6fbc4846
Altolele <89626132692@mail.ru>
2022-05-30 12:38:02 +0400

    Добавлен .gitignore Реализован Model

---
+++ GeoAppATM/GeoAppATM/Model/GeoJsonATM.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace GeoAppATM.Model
+{
+    /// <summary>
+    /// Класс содержащий информацию о банкомате
+    /// </summary>
+    public class GeoJsonATM
+    {
+        /// <summary>
+        /// Координаты банкомата
+        /// </summary>
+        [JsonPropertyName("geometry")]
+        public Geometry Geometry { get; set; }
+
+        /// <summary>
+        /// Идентификатор, оператор, баланс
+        /// </summary>
+        [JsonPropertyName("properties")]
+        public Properties Properties { get; set; }
+
+        /// <summary>
+        /// Сравнение двух банкоматов
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            return obj is GeoJsonATM atm &&
+                atm.Geometry.Coordinates[0] == Geometry.Coordinates[0] &&
+                atm.Geometry.Coordinates[1] == Geometry.Coordinates[1] &&
+                atm.Properties.Id == Properties.Id &&
+                atm.Properties.Operator == Properties.Operator &&
+                atm.Properties.Balance == Properties.Balance;
+        }
+
+        /// <summary>
+        /// Хеш-код банкомата по его идентификатору
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            try
+            {
+                return int.Parse(Properties.Id);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Класс для хранения координат
+    /// </summary>
+    public class Geometry
+    {
+        [JsonPropertyName("coordinates")]
+        public List<double> Coordinates { get; set; }
+    }
+
+    /// <summary>
+    /// Класс для хранения идентификатора, оператора, баланса
+    /// </summary>
+    public class Properties
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("operator")]
+        public string Operator { get; set; }
+
+        [JsonPropertyName("balance")]
+        public int Balance { get; set; }
+    }
+
+    /// <summary>
+    /// Тип данных для чтения банкоматов из geojson
+    /// </summary>
+    public class JsonATMList
+    {
+        [JsonPropertyName("features")]
+        public List<GeoJsonATM> ATMs { get; set; }
+    }
+}
---
+++ GeoAppATM/GeoAppATM/Model/JsonATM.cs
@@ -0,0 +1,18 @@
+﻿namespace GeoAppATM.Model
+{
+    /// <summary>
+    /// Класс для хранения идентификатора и баланса в Json
+    /// </summary>
+    public class JsonATM
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Баланс
+        /// </summary>
+        public int Balance { get; set; }
+    }
+}
