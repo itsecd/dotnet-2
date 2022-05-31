51320e86eefd01a65d72c5e16c213a617c2dec23
VladaHaustova <vlada.khaustova@yandex.ru>
2022-05-31 21:16:54 +0400

    Add getting ATM's from backend and showing them on map

--- ATMClient/wwwroot/js/script.js
+++ ATMClient/wwwroot/js/script.js
@@ -3,7 +3,7 @@ const url = 'https://localhost:44372/'
 var map = L.map('map').setView([53.195878, 50.100202], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: '<a href="https://github.com/vladahaustova">Vlada Khaustova</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
@@ -15,31 +15,35 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 // marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
-let ATMs = [];
+let selectedATMId = "";
+let markers = {};
 
 fetch(url + 'api/ATM', {})
 	.then(response => response.json())
 	.then(result => { 
 		console.log(result);
 		for (let atm of result) {
-			ATMs.push(atm);
-			console.log(atm);
-			L.marker([atm.coords.x, atm.coords.y]).addTo(map)
-				.bindPopup("<b>" + atm.bankName + "</b><br>Balance: " + atm.balance);
+			let marker = L.marker([atm.geometry.coordinates[1], atm.geometry.coordinates[0]]).addTo(map)
+				.bindPopup("<b>" + atm.properties.operator + "</b><br>Balance: " + atm.properties.balance + "<br><br><input class='input-new-balance' type='text'><br><button onclick='changeBalance()'>Поменять баланс</button><br><br>" + atm.properties.id)
+				.addEventListener("click", () => {
+					selectedATMId = atm.properties.id;
+				});
+			markers[atm.properties.id] = marker;
 		}
 	})
 	
 async function changeBalance() {
 	try {
-		const response = await fetch(url + 'api/ATM/gjsdp10288ca8g', {
+		const response = await fetch(url + 'api/ATM/' + selectedATMId, {
 		method: 'PUT', // или 'PUT'
-		body: 44444, // данные могут быть 'строкой' или {объектом}!
+		body: document.querySelector('.input-new-balance').value, // данные могут быть 'строкой' или {объектом}!
 		headers: {
 			'Content-Type': 'application/json'
 		}
 	});
 	const json = await response.json();
-	console.log('Успех:', JSON.stringify(json));
+	console.log('Успех:', json);
+	markers[json.properties.id].bindPopup("<b>" + json.properties.operator + "</b><br>Balance: " + json.properties.balance + "<br><br><input class='input-new-balance' type='text'><br><button onclick='changeBalance()'>Поменять баланс</button><br><br>" + json.properties.id)
 	} catch (error) {
 		console.error('Ошибка:', error);
 	}
