e7eaf194818d7873f503d61dadea64d60edd7d56
Валерия Косова <nishteityen@gmail.com>
2022-06-05 22:41:24 +0400

    Добавлено окно ввода логина

---
+++ Gomoku/GomokuClient/LoginWindow.xaml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GomokuClient
+{
+    public partial class LoginWindow : Window
+    {
+        public string Login { get; set; }
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayClick(object sender, RoutedEventArgs e)
+        {
+            Login = LoginTextBox.Text;
+        }
+    }
+}
