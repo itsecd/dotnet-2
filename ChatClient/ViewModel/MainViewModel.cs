07abed2906a627694e41c66441f3f71c7e61c093
Dryg1214 <pakazancev901@gmail.com>
2022-06-06 03:00:23 +0400

    Выполнена Очистка Кода

--- ChatClient/ViewModel/MainViewModel.cs
+++ ChatClient/ViewModel/MainViewModel.cs
@@ -2,10 +2,7 @@
 using ChatClient.Model;
 using ChatClient.Services;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
