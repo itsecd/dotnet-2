6056609ed756d42ca16b8664dd367fe0d7d2a09d
Altolele <89626132692@mail.ru>
2022-06-05 19:46:38 +0400

    реализация форм

--- GeoAppATM/GeoAppAtmClient/MainWindow.xaml.cs
+++ GeoAppATM/GeoAppAtmClient/MainWindow.xaml.cs
@@ -1,28 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GeoAppAtmClient.ViewModels;
+using Mapsui;
+using Mapsui.Layers;
+using Mapsui.Projections;
+using Mapsui.Providers;
+using Mapsui.Styles;
+using Mapsui.Tiling;
+using Mapsui.Utilities;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using IFeature = Mapsui.IFeature;
 
 namespace GeoAppAtmClient
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private async void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var viewModel = (MapViewModel)DataContext;
+            var atms = await viewModel.AtmRepository.GetAtmsAsync();
+
+            if (MapControl.Map == null) return;
+
+            MapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            MapControl.Navigator.NavigateTo(SphericalMercator.FromLonLat(new MPoint(51, 53)), 500);
+            MapControl.Info += MapControl_Info;
+
+            var atmLayer = new MemoryLayer("Atm")
+            {
+                IsMapInfoLayer = true,
+                DataSource = new MemoryProvider<IFeature>(atms.Select(atm =>
+                {
+                    var (x, y) = SphericalMercator.FromLonLat(atm.Latitude, atm.Longitude);
+
+                    var feature = new PointFeature(x, y);
+
+                    feature["id"] = atm.Id;
+                    feature["name"] = atm.Name;
+                    return feature;
+                })),
+
+                //Style = CreateSvgStyle("atm.svg", 0.5)
+            };
+
+            MapControl.Map.Layers.Add(atmLayer);
+        }
+
+        private async void MapControl_Info(object sender, Mapsui.UI.MapInfoEventArgs e)
+        {
+            if (e.MapInfo?.Feature == null) return;
+            var viewModel = (MapViewModel)DataContext;
+            await viewModel.ShowAtmInfo((string)e.MapInfo.Feature["id"]);
+        }
+
+        //private static SymbolStyle CreateSvgStyle(string embeddedResourcePath, double scale)
+        //{
+        //    var bitmapId = typeof(MainWindow).LoadSvgId(embeddedResourcePath);
+        //    return new SymbolStyle { BitmapId = bitmapId, SymbolScale = scale, SymbolOffset = new Offset(0.0, 0.5, true) };
+        //}
     }
 }
---
+++ GeoAppATM/GeoAppAtmClient/Views/AtmsView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using GeoAppAtmClient.ViewModels;
+
+namespace GeoAppAtmClient.Views
+{
+    public partial class AtmsView : Window
+    {
+        public AtmsView()
+        {
+            InitializeComponent();
+        }
+        public AtmsView(AtmsViewModel atmsViewModel)
+        {
+            InitializeComponent();
+            DataContext = atmsViewModel;
+        }
+    }
+}
