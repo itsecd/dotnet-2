c133d5a0afc9d91a1ea7a7b80237b4b80f33a1b5
Mityco <nice.materov@mail.ru>
2022-06-06 20:52:43 +0400

    Add ViewModel for AddReminderWindow

---
+++ TelegramBotClient/Model/EventReminder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotClient.Model
+{
+    public  class EventReminder
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime Time { get; set; }
+        public TimeSpan RepeatPeriod { get; set; }
+    }
+}
---
+++ TelegramBotClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotClient.ViewModels
+{
+    public class MainViewModel: ReactiveObject
+    {
+
+        public ReactiveCommand<Unit, Unit> AddCommand { get; }
+        public ReactiveCommand<Unit, Unit> EditCommand { get; }
+        public ReactiveCommand<Unit, Unit> RemoveCommand { get; }
+    }
+}
