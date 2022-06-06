4f7e05058a7065048c41ad8e833ae6d68773bbb3
Антонина Хвацкова <2019-01388@students.ssau.ru>
2022-06-06 19:04:15 +0400

    правки 2

--- src/TaskListKhvatskova/TaskListKhvatskova/TaskListKhvatskovaClient/ViewModels/MainWindowViewModel.cs
+++ src/TaskListKhvatskova/TaskListKhvatskova/TaskListKhvatskovaClient/ViewModels/MainWindowViewModel.cs
@@ -5,7 +5,7 @@ using System.Drawing;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
-using TaskListKhvatskova.Models;
+using TaskListKhvatskovaClient.Models;
 
 namespace TaskListKhvatskovaClient.ViewModels
 {
